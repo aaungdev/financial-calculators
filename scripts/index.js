2d799a815e79e8e0a99bef70a97f76625695438b
aaungdev <dhev.code@gmail.com>
2024-05-11 15:00:07 -0400

    update javascript and footer

--- scripts/index.js
+++ scripts/index.js
@@ -18,50 +18,51 @@ window.onload = function () {
 };
 
 function calculateAnnuityPV() {
-  var payment = parseFloat(document.getElementById("annuityPayment").value);
-  var interestRate =
-    parseFloat(document.getElementById("annuityInterestRate").value) / 100;
-  var years = parseFloat(document.getElementById("annuityYears").value);
-  var monthlyRate = interestRate / 12;
+  var payment = document.getElementById("annuityPayment").value;
+  var interestRate = document.getElementById("annuityInterestRate").value;
+  var years = document.getElementById("annuityYears").value;
+
+  var rateDecimal = interestRate / 100;
+  var monthlyRate = rateDecimal / 12;
   var periods = years * 12;
-  var presentValue =
-    payment * ((1 - Math.pow(1 + monthlyRate, -periods)) / monthlyRate);
-  document.getElementById("annuityPresentValue").innerText =
-    presentValue.toFixed(2);
+  var powCalculation = Math.pow(1 + monthlyRate, -periods);
+  var subtractFromOne = 1 - powCalculation;
+  var presentValue = payment * (subtractFromOne / monthlyRate);
+
+  document.getElementById("annuityPresentValue").innerText = presentValue.toFixed(2);
 }
 
 function calculateFutureValue() {
-  var deposit = parseFloat(document.getElementById("initialDeposit").value);
-  var interestRate =
-    parseFloat(document.getElementById("annualInterest").value) / 100;
-  var years = parseFloat(document.getElementById("depositYears").value);
-  var compoundingsPerYear = 365;
-  var futureValue =
-    deposit *
-    Math.pow(
-      1 + interestRate / compoundingsPerYear,
-      compoundingsPerYear * years,
-    );
+  var deposit = document.getElementById("initialDeposit").value;
+  var interestRate = document.getElementById("annualInterest").value;
+  var years = document.getElementById("depositYears").value;
+
+  var rateDecimal = interestRate / 100;
+  var dailyRate = rateDecimal / 365;
+  var compoundingPeriods = 365 * years;
+  var growthFactor = 1 + dailyRate;
+  var powGrowth = Math.pow(growthFactor, compoundingPeriods);
+  var futureValue = deposit * powGrowth;
   var totalInterest = futureValue - deposit;
+
   document.getElementById("futureValue").innerText = futureValue.toFixed(2);
-  document.getElementById("totalInterestEarned").innerText =
-    totalInterest.toFixed(2);
+  document.getElementById("totalInterestEarned").innerText = totalInterest.toFixed(2);
 }
 
 function calculateMortgage() {
-  var principal = parseFloat(
-    document.getElementById("mortgagePrincipal").value,
-  );
-  var interestRate =
-    parseFloat(document.getElementById("mortgageInterestRate").value) / 100;
-  var years = parseFloat(document.getElementById("mortgageLoanTerm").value);
-  var monthlyRate = interestRate / 12;
+  var principal = document.getElementById("mortgagePrincipal").value;
+  var interestRate = document.getElementById("mortgageInterestRate").value;
+  var years = document.getElementById("mortgageLoanTerm").value;
+
+  var rateDecimal = interestRate / 100;
+  var monthlyRate = rateDecimal / 12;
   var payments = years * 12;
-  var monthlyPayment =
-    (principal * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -payments));
-  var totalInterest = monthlyPayment * payments - principal;
-  document.getElementById("mortgageMonthlyPayment").innerText =
-    monthlyPayment.toFixed(2);
-  document.getElementById("mortgageTotalInterest").innerText =
-    totalInterest.toFixed(2);
+  var powMinusPayments = Math.pow(1 + monthlyRate, -payments);
+  var oneMinusPow = 1 - powMinusPayments;
+  var monthlyPayment = (principal * monthlyRate) / oneMinusPow;
+  var totalPaid = monthlyPayment * payments;
+  var totalInterest = totalPaid - principal;
+
+  document.getElementById("mortgageMonthlyPayment").innerText = monthlyPayment.toFixed(2);
+  document.getElementById("mortgageTotalInterest").innerText = totalInterest.toFixed(2);
 }
